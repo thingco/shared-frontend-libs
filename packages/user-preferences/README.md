a3bcd50664d8e9be15510a97afb5b70cf505a0ba
DanCouper <danielcouper81@googlemail.com>
2021-05-01 00:44:25 +0100

    feat(user-preferences): built out user preferences
    - Add @thingco/shared-types private package to allow types for the user preferences to be shared. This allows
    - Add @thingco/user-preferences-storage-web and @thingco/user-preferences-storage-native, which are thin wrappers
      around localStorage and AsyncStorage respectively, exposing getPreferences, getPreference and setPreference methods
      on an object created with default prefs. This allows
    - Add @thingco/user-preferences, which is a React context provider that accepts a store that conforms to the interface
      defined in the shared types repo + exposes a hook for access to the user preferences themselve + a set function
      that updates them & persists the change to whichever storage option is provided.

--- packages/user-preferences/README.md
+++ packages/user-preferences/README.md
@@ -2,14 +2,33 @@
 
 ## Overview
 
-An interface for storing user preferences in React/React Native apps, with a provider & a hook.
+An React/React Native interface for storing user preferences ThingCo apps. Provides a provider (+ a hook for access/updating).
+
+## I'm using Typescript. Why can't I change what the user preferences actually are (or, say, add to them)‽
+
+You're in the wrong place. Go to `@thingco/shared-types` and do your adjustments there. This library doesn't care what the actual preferences are as long as they match what is defined in that package. If you're trying to set a preference to something that isn't defined there I'm afraid you're probably going to have a bad time.
 
 ## Installation
 
-Ensure your project is set up to allow pulling `@thingco/` namespaced packages from GitHub rather than NPM, then:
+Ensure your project is set up to allow pulling `@thingco/` namespaced packages from GitHub rather than NPM.
 
 ```
-yarn add @theo/user-preferences@0.1.0
+yarn add @thingco/user-preferences@0.1.0
+```
+
+It is necessary to provide a store implementation. You can build one yourself -- see the respective `user-preferences-store-` implementations as an example, they're very simple. But more likely
+you will want to just use the web storage API or the React Native API. For web, you'll need React installed, then:
+
+```
+yarn add @thingco/user-preferences-store-web@0.1.0
+```
+
+For React native, you'll need React, React Native and AsyncStorage (`@react-native-async-storage/async-storage`) installed. Then:
+
+```
+yarn add @thingco/user-preferences-store-native@0.1.0
 ```
 
 ## Usage
+
+TODO
--- packages/user-preferences/src/index.tsx
+++ packages/user-preferences/src/index.tsx
@@ -1,1 +1,128 @@
-console.log("hello world");
+import React from "react";
+
+import type {
+	DistanceUnitPreference,
+	PreferenceStorageKey,
+	Storage,
+	TimeDisplayPreference,
+	UserPreferences,
+} from "@thingco/shared-types";
+
+const UserPrefsRead = React.createContext<UserPreferences | null>(null);
+const UserPrefsWrite = React.createContext<{
+	updateUserPreference: <K extends keyof UserPreferences>(
+		key: K,
+		value: UserPreferences[K]
+	) => void;
+} | null>(null);
+
+type prefUpdate<K extends keyof UserPreferences> = {
+	type: "UserPreferenceUpdate";
+	key: K;
+	value: UserPreferences[K];
+};
+
+function userPrefsReducer<K extends keyof UserPreferences>(
+	state: UserPreferences,
+	update: prefUpdate<K>
+): UserPreferences {
+	return { ...state, [update.key]: update.value };
+}
+
+const UserPrefsInitialisedProvider = ({
+	children,
+	store,
+	initialPrefs,
+}: UserPrefsProviderProps & { initialPrefs: UserPreferences }) => {
+	const [persistState, setPersistState] = React.useState<prefUpdate<keyof UserPreferences> | null>(
+		null
+	);
+	const [localPreferences, updateLocalPreferences] = React.useReducer(
+		userPrefsReducer,
+		initialPrefs
+	);
+
+	const updateUserPreference = <K extends keyof UserPreferences>(
+		key: K,
+		value: UserPreferences[K]
+	) => {
+		const updateObject: prefUpdate<K> = { type: "UserPreferenceUpdate", key, value };
+		setPersistState(updateObject);
+		updateLocalPreferences(updateObject);
+	};
+
+	React.useEffect(() => {
+		const persist = async () => {
+			if (persistState) {
+				await store.setPreference(persistState.key, persistState.value);
+				setPersistState(null);
+			}
+		};
+
+		if (persistState !== null) persist();
+	}, [persistState]);
+
+	return (
+		<UserPrefsRead.Provider value={localPreferences}>
+			<UserPrefsWrite.Provider value={{ updateUserPreference }}>{children}</UserPrefsWrite.Provider>
+		</UserPrefsRead.Provider>
+	);
+};
+
+export interface UserPrefsProviderProps {
+	children: React.ReactNode;
+	store: Storage;
+}
+
+export const UserPrefsProvider = ({ children, store }: UserPrefsProviderProps): JSX.Element => {
+	const [initialPrefs, setInitialPrefs] = React.useState<UserPreferences | null>(null);
+	React.useEffect(() => {
+		const init = async () => {
+			const prefs = await store.getPreferences();
+			setInitialPrefs(prefs);
+		};
+		init();
+	}, []);
+
+	return initialPrefs ? (
+		<UserPrefsInitialisedProvider store={store} initialPrefs={initialPrefs}>
+			{children}
+		</UserPrefsInitialisedProvider>
+	) : (
+		<></>
+	);
+};
+
+export function usePrefs(): UserPreferences & {
+	setPref: <K extends keyof UserPreferences>(key: K, value: UserPreferences[K]) => void;
+} {
+	const prefs = React.useContext(UserPrefsRead);
+	const updater = React.useContext(UserPrefsWrite);
+
+	if (!prefs || !updater) {
+		throw new Error(
+			"The UserPreferencesProvider is not initialised in the component tree. Either this function is being called from outside the component tree that contains the UserPreferencesProvider, or the state of that provider has not initiialised correctly."
+		);
+	}
+
+	return {
+		...prefs,
+		setPref: updater.updateUserPreference,
+	};
+}
+
+/**
+ * Re-export the shared types.
+ *
+ * BE VERY CAREFUL WITH THIS. Because the types are shared, if re-exported from another package as well,
+ * there is the distinct possiblity of conflicts (even though the types are actually exactly the same).
+ *
+ * REVIEW publish the shared types?
+ */
+export type {
+	DistanceUnitPreference,
+	PreferenceStorageKey,
+	Storage,
+	TimeDisplayPreference,
+	UserPreferences,
+};
