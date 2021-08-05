70b83658e00fa28633825c77e459aba1b8a20612
Carlo Conner-Hill <carlo.connerhill@ThingCos-MacBook-Pro.local>
2021-08-05 11:06:53 +0100

    add native button

---
+++ packages/react-component-library/src/Button.native.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { TextStyle, TouchableOpacity, ViewStyle, Text, TouchableOpacityProps } from "react-native";
+
+interface ButtonProps extends TouchableOpacityProps {
+	children?: React.ReactElement | string;
+	isDisabled?: boolean;
+	variant?: string;
+	style?: ViewStyle[] | ViewStyle;
+	textStyle?: TextStyle[] | TextStyle;
+}
+
+export const Button = ({
+	children,
+	variant = "",
+	isDisabled = false,
+	style = [],
+	textStyle = [],
+	...props
+}: ButtonProps) => {
+	const custStyle = style instanceof Array ? style : [style];
+	const custTextStyle = textStyle instanceof Array ? textStyle : [textStyle];
+
+	return (
+		<TouchableOpacity
+			style={[
+				isDisabled && {
+					borderRadius: 10,
+					height: 50,
+					padding: 5,
+					marginVertical: 5,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "#199a8d",
+					borderColor: "#199a8d",
+				},
+				...custStyle,
+			]}
+			disabled={isDisabled}
+			{...props}
+		>
+			<Text
+				style={[
+					isDisabled && {
+						borderRadius: 10,
+						height: 50,
+						padding: 5,
+						marginVertical: 5,
+						justifyContent: "center",
+						alignItems: "center",
+						backgroundColor: "#199a8d",
+						borderColor: "#199a8d",
+						opacity: 0.5,
+					},
+					...custTextStyle,
+				]}
+			>
+				{children}
+			</Text>
+		</TouchableOpacity>
+	);
+};
