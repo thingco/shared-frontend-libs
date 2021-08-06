1c7a3a3f98fe73f8e7277f359ab186526c0132a6
Carlo Conner-Hill <carlo.connerhill@ThingCos-MacBook-Pro.local>
2021-08-06 12:47:06 +0100

    working locally

--- packages/react-component-library/src/Button/Button.native.tsx
+++ packages/react-component-library/src/Button/Button.native.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { TextStyle, TouchableOpacity, ViewStyle, Text, TouchableOpacityProps } from "react-native";
+import {
+	TextStyle,
+	TouchableOpacity,
+	ViewStyle,
+	Text,
+	TouchableOpacityProps,
+} from "react-native";
 
 interface ButtonProps extends TouchableOpacityProps {
 	children?: React.ReactElement | string;
@@ -23,7 +29,7 @@ export const Button = ({
 	return (
 		<TouchableOpacity
 			style={[
-				isDisabled && {
+				{
 					borderRadius: 10,
 					height: 50,
 					padding: 5,
@@ -33,6 +39,9 @@ export const Button = ({
 					backgroundColor: "#199a8d",
 					borderColor: "#199a8d",
 				},
+				isDisabled && {
+					opacity: 0.5,
+				},
 				...custStyle,
 			]}
 			disabled={isDisabled}
@@ -40,15 +49,13 @@ export const Button = ({
 		>
 			<Text
 				style={[
+					{
+						fontSize: 18,
+						lineHeight: 22,
+						fontWeight: "700",
+						color: "#ffffff",
+					},
 					isDisabled && {
-						borderRadius: 10,
-						height: 50,
-						padding: 5,
-						marginVertical: 5,
-						justifyContent: "center",
-						alignItems: "center",
-						backgroundColor: "#199a8d",
-						borderColor: "#199a8d",
 						opacity: 0.5,
 					},
 					...custTextStyle,
