5b5f9a626b92620e6288b466bb30ed39ec3fcf5e
Carlo Conner-Hill <carlo.connerhill@ThingCos-MacBook-Pro.local>
2021-08-24 11:37:32 +0100

    work around theme typing as any, build and bump

--- packages/component-library-native/src/util/styleBuilder.tsx
+++ packages/component-library-native/src/util/styleBuilder.tsx
@@ -6,37 +6,36 @@ import { useTheme } from "../Provider/ThemeProvider";
 
 export const variantToTheme = ({ component, styles }: variantProps): any[] => {
 	const { theme } = useTheme();
+	const themeWorkAround = theme as any;
 	const variants = styles.split(" ");
 	const variantStyles: any[] = [];
 	const themeRef =
 		component === "button"
-			? theme.buttons
+			? (theme.buttons as any)
 			: component === "text"
-			? theme.typography
+			? (theme.typography as any)
 			: component === "view"
-			? theme.views
+			? (theme.views as any)
 			: component === "input"
-			? theme.inputs
+			? (theme.inputs as any)
 			: component === "carousel"
-			? theme.carousel
-			: component === "label" && theme.typography?.inputs;
+			? (theme.carousel as any)
+			: component === "label" && (theme.typography?.inputs as any);
 	variants.map((v) => {
-		if (themeRef ? [v] : {}) {
-			variantStyles.push(themeRef ? [v] : {});
-			// } else if (
-			// 	(component === "text" || component === "label") && theme.typography?.fontSize ? [v] : {}
-			// ) {
-			// 	variantStyles.push(themeRef?.fontSize[v]);
-			// } else if (component === "text" && themeRef?.buttons[v]) {
-			// 	variantStyles.push(themeRef?.buttons[v]);
-		} else if (theme.colors ? [v] : {}) {
+		if (themeRef[v]) {
+			variantStyles.push(themeRef[v]);
+		} else if ((component === "text" || component === "label") && themeRef.fontSize[v]) {
+			variantStyles.push(themeRef?.fontSize[v]);
+		} else if (component === "text" && themeRef?.buttons[v]) {
+			variantStyles.push(themeRef?.buttons[v]);
+		} else if (themeWorkAround.colors[v]) {
 			if (component === "text") {
-				variantStyles.push({ color: theme.colors ? [v] : {} });
+				variantStyles.push({ color: themeWorkAround.colors[v] });
 			} else {
-				variantStyles.push({ backgroundColor: theme.colors ? [v] : {} });
+				variantStyles.push({ backgroundColor: themeWorkAround.colors[v] });
 			}
-		} else if (theme.spacing ? [v] : {}) {
-			variantStyles.push(theme.spacing ? [v] : {});
+		} else if (themeWorkAround.spacing[v]) {
+			variantStyles.push(themeWorkAround.spacing[v]);
 		} else if (v != "") {
 			console.warn("No style found for variant:", v, " in ", component);
 		}
