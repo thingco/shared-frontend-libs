bcc92a846bdc31bac53279ef04a952b32aefd54b
DanCouper <danielcouper81@googlemail.com>
2021-10-18 16:53:28 +0100

    build: remove husky, add commitizen, add .git-hooks dir + setup script, add minor change to auth
    Husky (the node githooks library) seems somewhat pointless. Linting commit messages, not so much. So
    remove Husky + the githooks it created, add and set up commitizen, create a .git-hooks directory and
    alter the git config to point to it, add the recommended commitizen script to it to have commitizen
    run on commit.
    
    A minor change has been made to the authentication library -- the two new config values
    (allowedOtpRetries and pinLength) are now available from the useAuthentication hook. This is
    non-breaking and is being used as a test of commitizen.

--- packages/authentication/src/react/AuthSystemProvider.tsx
+++ packages/authentication/src/react/AuthSystemProvider.tsx
@@ -6,8 +6,19 @@ import { contextSelectors, currentStateSelector } from "./selectors";
 import { machine } from "../auth-system";
 
 import type { ReactNode } from "react";
-import type { DeviceSecurityType, LoginFlowType, AuthInterpreter, AuthState } from "../types";
+import type {
+	DeviceSecurityType,
+	LoginFlowType,
+	AuthInterpreter,
+	AuthState
+} from "../types";
 
+/**
+ * The sole context value stored in the `AuthProvider` is the interpreted FSM.
+ *
+ * @category React
+ * @internal
+ */
 const AuthProviderContext = createContext<{
 	authenticator: AuthInterpreter;
 } | null>(null);
@@ -16,48 +27,54 @@ const AuthProviderContext = createContext<{
  * @category React
  */
 export type AuthProviderProps = {
+	/**
+	 * The `AuthProvider` must have children: it's a context provider.
+	 */
 	children: ReactNode;
 	/**
-	 * The preconfigured device deviceSecurityType. This is configured on a per-client basis,
-	 * so is fixed for each specific app.
+	 * The preconfigured device deviceSecurityType. This is configured on a
+	 * per-client basis, so is fixed for each specific app.
 	 */
 	deviceSecurityType: DeviceSecurityType;
 	/**
-	 * The preconfigured device loginFlowType. This is configured on a per-client basis,
-	 * so is fixed for each specific app.
+	 * The preconfigured device loginFlowType. This is configured on a per-client
+	 * basis, so is fixed for each specific app.
 	 */
 	loginFlowType: LoginFlowType;
 	/**
-	 * If an OTP login flow is being used, defines how many retries are allowed before
-	 * a user is bumped back to the start of the login process. Allows altering the
-	 * machine default value.
+	 * If an OTP login flow is being used, defines how many retries are allowed
+	 * before a user is bumped back to the start of the login process. Allows
+	 * altering the machine default value.
 	 */
 	allowedOtpRetries?: number;
 	/**
-	 * If PIN device security is active, defines the length of the PIN. Allows altering the
-	 * machine default value.
+	 * If PIN device security is active, defines the length of the PIN. Allows
+	 * altering the machine default value.
 	 */
 	pinLength?: number;
 	/**
-	 * Enables the `@xstate/inspect` visual diagram tool. This would be installed at point of use:
-	 * in the entry point of an app. NOTE that this only works currently in a web context: attempting
-	 * to use it with a React Native app will cause critical errors, as it requires the `window` object.
+	 * Enables the `@xstate/inspect` visual diagram tool. This would be installed
+	 * at point of use: in the entry point of an app. NOTE that this only works
+	 * currently in a web context: attempting to use it with a React Native app
+	 * will cause critical errors, as it requires the `window` object.
 	 */
 	useDevTools?: boolean;
 	/**
-	 * A function that will be given the state object after every event, and will dumps that object
-	 * somewhere. For example, the console -- `console.log` is a perfectly valid eventSink.
+	 * A function that will be given the state object after every event, and will
+	 * dump that object somewhere. For example, the console -- `console.log` is a
+	 * perfectly valid eventSink.
 	 *
 	 * This is useful for two reasons:
 	 * 1. enables logging of everything that happens in the app, a la Redux logger
-	 * 2. enables pushing records of events/state to somewhere controlled during tests
+	 * 2. enables pushing records of events/state to somewhere controlled
+	 * during tests
 	 */
 	eventSink?: (state: AuthState) => void;
 };
 
 /**
- * The React context provider that holds an interpreted version of the auth system.
- * everything in the React part of the library relies on this.
+ * The React context provider that holds an interpreted version of the auth
+ * system. Everything in the React part of the library relies on this.
  *
  * @category React
  */
@@ -77,13 +94,17 @@ export function AuthProvider({
 		loginFlowType,
 		pinLength,
 	});
-	const authenticator = useInterpret(authenticationSystem, { devTools: useDevTools }, (state) => {
-		if (eventSink) {
-			if (state.changed) {
-				eventSink(state);
+	const authenticator = useInterpret(
+		authenticationSystem,
+		{ devTools: useDevTools },
+		(state) => {
+			if (eventSink) {
+				if (state.changed) {
+					eventSink(state);
+				}
 			}
-		}
-	});
+		},
+	);
 
 	return (
 		<AuthProviderContext.Provider value={{ authenticator }}>
@@ -93,8 +114,8 @@ export function AuthProvider({
 }
 
 /**
- * This hook is not to be used externally, but is what powers the exposed hooks for
- * each state.
+ * This hook is not to be used externally, but is what powers the exposed
+ * hooks for each state.
  *
  * @category React
  * @internal
@@ -114,21 +135,45 @@ export function useAuthInterpreter() {
  *
  * @remarks
  * This is not designed for use within any of the stage-specific hooks, it is
- * a utility for use in components in-app. For example profile screens, to show/hide
- * menu items depending upon whther the operations they trigger are actually
- * available.
+ * a utility for use in components in-app. For example profile screens, to
+ * show/hide menu items depending upon whther the operations they trigger are
+ * actually available.
  *
  * @category React
  */
 export function useAuthProvider() {
 	const authSystem = useAuthInterpreter();
-	const currentState = useSelector(authSystem, currentStateSelector);
-	const loginFlowType = useSelector(authSystem, contextSelectors.loginFlowType);
-	const deviceSecurityType = useSelector(authSystem, contextSelectors.deviceSecurityType);
+
+	const allowedOtpRetries = useSelector(
+		authSystem,
+		contextSelectors.allowedOtpRetries
+	);
+
+	const currentState = useSelector(
+		authSystem,
+		currentStateSelector
+	);
+
+	const deviceSecurityType = useSelector(
+		authSystem,
+		contextSelectors.deviceSecurityType
+	);
+
+	const loginFlowType = useSelector(
+		authSystem,
+		contextSelectors.loginFlowType
+	);
+
+	const pinLength = useSelector(
+		authSystem,
+		contextSelectors.pinLength
+	);
 
 	return {
+		allowedOtpRetries,
 		currentState,
-		loginFlowType,
 		deviceSecurityType,
+		loginFlowType,
+		pinLength,
 	};
 }
