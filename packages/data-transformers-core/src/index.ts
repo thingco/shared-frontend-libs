0de5c7d4456f8728f9e39a967bac7fef4b2491b9
DanCouper <danielcouper81@googlemail.com>
2021-12-10 01:14:01 +0000

    add reexports to data-transformers-core package entry point
    Add additional exports of calculators, converters and formatters from the index.ts file under respective namespaces to enable easily importing them all at once and iterating over them in the data-transformers-react package.

--- packages/data-transformers-core/src/index.ts
+++ packages/data-transformers-core/src/index.ts
@@ -3,3 +3,7 @@ export * from "./converters";
 export * from "./date-utils";
 export * from "./formatters";
 export * from "./math-utils";
+
+export * as calculators from "./calculators";
+export * as converters from "./converters";
+export * as formatters from "./formatters";
